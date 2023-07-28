d65784fa6204e3ca0cd6c48fb6ba1bd5405e3bf8
Max <maxmwangikenya@gmail.com>
2023-07-28 23:52:26 +0300

    Added footer and preliminary share

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
+import Footer from './Components/Footer';
+import Share from './Components/Share';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [songData, setsongData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/shares")
+      .then(response => response.json())
+      .then(data => setsongData(data))
+      .catch(error => console.error(error));
+  }, []);
   return (
-    <div className="App">
-    
-    </div>
+<>
+<Share />
+<Footer />
+</>
   );
 }
 
